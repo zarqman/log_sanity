071bb65531b60d7ff5970c4a878fc4e52824a94f
thomas morgan <tm@iprog.com>
2023-10-09 15:33:33 -0600

    rework middleware for updated instrumentation api
    parallels changes in Rails::Rack::Logger in 7.1.

--- lib/log_sanity/middleware/request_logger.rb
+++ lib/log_sanity/middleware/request_logger.rb
@@ -9,15 +9,21 @@ module LogSanity
       request = ActionDispatch::Request.new(env)
 
       conditionally_silence(request) do |silence|
+        payload = {env: env, request: request, silence: silence}
+        handle = start(payload)
         begin
-          start(request: request)
-          resp = @app.call(env)
-          resp[2] = Rack::BodyProxy.new(resp[2]) do
-            finish(env: env, request: request, response: resp, silence: silence)
+          status, headers, body = response = @app.call(env)
+          payload[:response] = response
+          body = Rack::BodyProxy.new(body){ handle.finish }
+          if response.frozen?
+            [status, headers, body]
+          else
+            response[2] = body
+            response
           end
-          resp
         rescue Exception => e
-          finish(env: env, request: request, exception: e, silence: silence)
+          payload[:exception] = e
+          handle.finish
           raise e
         end
       end
@@ -39,15 +45,12 @@ module LogSanity
 
     private
 
-    def start(params)
+    def start(payload)
       LogSanity.reset_fields
       instrumenter = ActiveSupport::Notifications.instrumenter
-      instrumenter.start 'request.action_dispatch', params
-    end
-
-    def finish(params)
-      instrumenter = ActiveSupport::Notifications.instrumenter
-      instrumenter.finish 'request.action_dispatch', params
+      instrumenter.build_handle('request.action_dispatch', payload).tap do |handle|
+        handle.start
+      end
     end
 
     def silence_path?(request)
